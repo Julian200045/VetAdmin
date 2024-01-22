119cd1f873bfa1cbdd63f551b5e9586041b7830b
Julian <jcarretto@frba.utn.edu.ar>
2024-01-22 13:19:19 -0300

    Initial

---
+++ VetAdmin.Api/Controllers/Test.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using VetAdmin.Infrastructure.Data;
+
+namespace VetAdmin.Api.Controllers
+{
+    public class Test
+    {
+        ApplicationDbContext context;
+        public Test(ApplicationDbContext applicationDbContext) { this.context = applicationDbContext; }
+        public void CheckConnection()
+        {
+            try
+            {
+                context.Database.EnsureCreated();
+            }
+            catch (SqlException)
+            {
+                Console.Write(false.ToString());
+            }
+            Console.Write(true.ToString());
+        }
+    }
+}
